a6287bb3b53d96e1f3e9c7047491083c24c0e54e
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-07-11 11:37:55 -0700

    Hack native to death

--- android/app/src/main/java/com/mxdbmobile/MainApplication.java
+++ android/app/src/main/java/com/mxdbmobile/MainApplication.java
@@ -3,8 +3,8 @@ package com.mxdbmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.dylanvann.fastimage.FastImageViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FastImageViewPackage(),
             new VectorIconsPackage(),
+            new FastImageViewPackage(),
             new RNGestureHandlerPackage()
       );
     }
