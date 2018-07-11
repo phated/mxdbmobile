6844828894f203e6672cfe6d450caa198ed311d2
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-07-11 11:42:34 -0700

    attempt react-native upgrade to reset project

--- android/app/src/main/java/com/mxdbmobile/MainApplication.java
+++ android/app/src/main/java/com/mxdbmobile/MainApplication.java
@@ -3,9 +3,6 @@ package com.mxdbmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.dylanvann.fastimage.FastImageViewPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new FastImageViewPackage(),
-            new RNGestureHandlerPackage()
+          new MainReactPackage()
       );
     }
 
