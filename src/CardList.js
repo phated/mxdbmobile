2c209804038ca1b1e5930eabc9465b0fabda3d9a
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-07-19 13:15:04 -0700

    Move ListOfCards to CardList

--- src/CardList.js
+++ src/CardList.js
@@ -1,11 +1,18 @@
 'use strict';
 
+var Block = require("bs-platform/lib/js/block.js");
 var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
+var Caml_int32 = require("bs-platform/lib/js/caml_int32.js");
 var Json_decode = require("@glennsl/bs-json/src/Json_decode.bs.js");
+var ReasonReact = require("reason-react/src/ReasonReact.js");
 var Card$Mxdbmobile = require("./card/Card.js");
 var Event$Mxdbmobile = require("./card/Event.js");
 var Battle$Mxdbmobile = require("./card/Battle.js");
+var Colors$Mxdbmobile = require("./Colors.js");
+var View$BsReactNative = require("bs-react-native/src/components/view.js");
+var Style$BsReactNative = require("bs-react-native/src/style.js");
 var Character$Mxdbmobile = require("./card/Character.js");
+var FlatList$BsReactNative = require("bs-react-native/src/components/flatList.js");
 
 var empty_000 = /* characters : array */[];
 
@@ -41,7 +48,98 @@ function decode(json) {
         ];
 }
 
+var container = Style$BsReactNative.style(/* :: */[
+      Style$BsReactNative.flex(1),
+      /* [] */0
+    ]);
+
+var separator = Style$BsReactNative.style(/* :: */[
+      Style$BsReactNative.flex(1.0),
+      /* :: */[
+        Style$BsReactNative.height(/* Pt */Block.__(0, [1.0])),
+        /* :: */[
+          Style$BsReactNative.backgroundColor(Colors$Mxdbmobile.Css[/* gray */2]),
+          /* [] */0
+        ]
+      ]
+    ]);
+
+var Styles = /* module */[
+  /* container */container,
+  /* separator */separator
+];
+
+function itemSeparatorComponent(param) {
+  return FlatList$BsReactNative.separatorComponent((function () {
+                return ReasonReact.element(/* None */0, /* None */0, View$BsReactNative.make(/* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* Some */[separator], /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0)(/* array */[]));
+              }), param);
+}
+
+function renderItem(param) {
+  return FlatList$BsReactNative.renderItem((function (bag) {
+                var card = bag[/* item */0];
+                switch (card.tag | 0) {
+                  case 0 : 
+                      var match = card[0];
+                      return ReasonReact.element(/* None */0, /* None */0, Character$Mxdbmobile.make(match[/* title */1], match[/* subtitle */2], match[/* trait */3], match[/* mp */4], match[/* stats */5], match[/* image */7], match[/* effect */6], /* array */[]));
+                  case 1 : 
+                      var match$1 = card[0];
+                      return ReasonReact.element(/* None */0, /* None */0, Event$Mxdbmobile.make(match$1[/* title */1], match$1[/* mp */2], match$1[/* image */4], match$1[/* effect */3], /* array */[]));
+                  case 2 : 
+                      var match$2 = card[0];
+                      return ReasonReact.element(/* None */0, /* None */0, Battle$Mxdbmobile.make(match$2[/* title */1], match$2[/* mp */2], match$2[/* stat */3], match$2[/* image */5], match$2[/* effect */4], /* array */[]));
+                  
+                }
+              }), param);
+}
+
+function getItemLayout(_, idx) {
+  return {
+          length: 170,
+          offset: Caml_int32.imul(170, idx),
+          index: idx
+        };
+}
+
+function getUid(card) {
+  return card[0][/* uid */0];
+}
+
+var component = ReasonReact.statelessComponent("CardList");
+
+function make(cards, _) {
+  return /* record */[
+          /* debugName */component[/* debugName */0],
+          /* reactClassInternal */component[/* reactClassInternal */1],
+          /* handedOffState */component[/* handedOffState */2],
+          /* willReceiveProps */component[/* willReceiveProps */3],
+          /* didMount */component[/* didMount */4],
+          /* didUpdate */component[/* didUpdate */5],
+          /* willUnmount */component[/* willUnmount */6],
+          /* willUpdate */component[/* willUpdate */7],
+          /* shouldUpdate */component[/* shouldUpdate */8],
+          /* render */(function () {
+              var sections = toArray(cards);
+              return ReasonReact.element(/* None */0, /* None */0, View$BsReactNative.make(/* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* Some */[container], /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0)(/* array */[ReasonReact.element(/* None */0, /* None */0, FlatList$BsReactNative.make(sections, renderItem, (function (card, _) {
+                                            return card[0][/* uid */0];
+                                          }), /* Some */[itemSeparatorComponent], /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* Some */[getItemLayout], /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0, /* None */0)(/* array */[]))]));
+            }),
+          /* initialState */component[/* initialState */10],
+          /* retainedProps */component[/* retainedProps */11],
+          /* reducer */component[/* reducer */12],
+          /* subscriptions */component[/* subscriptions */13],
+          /* jsElementWrapped */component[/* jsElementWrapped */14]
+        ];
+}
+
 exports.empty = empty;
 exports.toArray = toArray;
 exports.decode = decode;
-/* Event-Mxdbmobile Not a pure module */
+exports.Styles = Styles;
+exports.itemSeparatorComponent = itemSeparatorComponent;
+exports.renderItem = renderItem;
+exports.getItemLayout = getItemLayout;
+exports.getUid = getUid;
+exports.component = component;
+exports.make = make;
+/* container Not a pure module */
--- src/CardList.re
+++ src/CardList.re
@@ -22,3 +22,64 @@ let decode = json => {
   battles:
     json |> Json.Decode.field("battles", Json.Decode.array(Battle.decoder)),
 };
+
+module Styles = {
+  open BsReactNative.Style;
+  let container = style([flex(1.)]);
+
+  let separator =
+    style([
+      flex(1.0),
+      height(1.0 |. Pt),
+      backgroundColor(Colors.Css.gray),
+      /* marginLeft(16.0 |. Pt), */
+    ]);
+};
+let itemSeparatorComponent =
+  BsReactNative.FlatList.separatorComponent(_ =>
+    BsReactNative.(<View style=Styles.separator />)
+  );
+let renderItem =
+  BsReactNative.FlatList.renderItem(bag => {
+    let card: Card.t = bag.item;
+    switch (card) {
+    | Character({title, subtitle, trait, mp, stats, image, effect}) =>
+      <Character title subtitle trait mp stats image effect />
+    | Event({title, mp, image, effect}) => <Event title mp image effect />
+    | Battle({title, mp, stat, image, effect}) =>
+      <Battle title mp stat image effect />
+    };
+  });
+
+let getItemLayout = (_data, idx) => {
+  "length": 170,
+  "offset": 170 * idx,
+  "index": idx,
+};
+
+let getUid = card =>
+  switch (card) {
+  | Card.Character(character) => character.uid
+  | Card.Event(event) => event.uid
+  | Card.Battle(battle) => battle.uid
+  };
+
+let component = ReasonReact.statelessComponent("CardList");
+
+let make = (~cards, _children) => {
+  ...component,
+  render: self => {
+    open BsReactNative;
+    let sections = toArray(cards);
+
+    <View style=Styles.container>
+      <FlatList
+        data=sections
+        getItemLayout
+        keyExtractor=((card, _idx) => getUid(card))
+        renderItem
+        itemSeparatorComponent
+      />
+    </View>;
+  },
+};
