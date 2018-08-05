e9017173bcfd6e23235165ac0f64267c5c5c815f
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-08-04 17:05:40 -0700

    Add CodePush for Android

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
-import { run } from './src/Main.bs.js';
+import codePush from "react-native-code-push";
 
-AppRegistry.registerComponent('mxdbmobile', () => run);
+import { root as RootComponent } from './src/Main.bs.js';
+
+AppRegistry.registerComponent('mxdbmobile', () => codePush(RootComponent));
