82d20ca743026c8a4cb5e043759181ff75389c73
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-09-10 16:13:43 -0700

    Insert the Alpha deployment key to receive updates

--- android/app/src/main/java/com/mxdbmobile/MainApplication.java
+++ android/app/src/main/java/com/mxdbmobile/MainApplication.java
@@ -34,7 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
+            new CodePush("JgnR5qyRUYUkHS-MP163DPosCQRHByHHz_NOQ", getApplicationContext(), BuildConfig.DEBUG),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this),
